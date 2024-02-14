66cd06d7cae62c06d23310fc9df99f7c90e8b71b
Baptiste Asselin <baptiste.asselin@soprasteria.com>
2024-02-14 15:33:03 +0100

    removing 'private' function from row.h

--- includes/row.h
+++ includes/row.h
@@ -19,10 +19,6 @@ typedef struct {
 
 row_t create_table_row(table_metadata_t* table);
 void free_table_row(row_t* row);
-int get_data(row_t* row, size_t field, char** data);
-int get_data_from_name(row_t* row, const char* field, char** data);
-int put_data(row_t* row, size_t field, const char* data);
-int put_data_from_name(row_t* row, const char* field, const char* data);
 int put_integer(row_t* row, const char* field, uint64_t data);
 int put_string(row_t* row, const char* field, const char* data);
 int get_integer(row_t* row, const char* field, uint64_t* data);
